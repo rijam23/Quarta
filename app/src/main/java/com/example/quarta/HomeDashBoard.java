c6a52a489e98aed5b0353cd68e13d7948c95bc7a
jex <rijam23>
2021-07-31 22:20:23 +0800

    added automatic greetings

--- app/src/main/java/com/example/quarta/HomeDashBoard.java
+++ app/src/main/java/com/example/quarta/HomeDashBoard.java
@@ -6,6 +6,8 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.app.Activity;
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.graphics.Color;
+import android.os.Build;
 import android.os.Bundle;
 import android.view.MenuItem;
 import android.view.View;
@@ -54,13 +56,27 @@ public class HomeDashBoard extends AppCompatActivity {
         pressedTime = System.currentTimeMillis();
 
     }
-
+ 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home_dash_board);
+/////////////////////Transparent Status Bar//////////////////////////////////////////////////////
+        //getSupportActionBar().hide();//its hide actionbar
 
+        if (Build.VERSION.SDK_INT>=19 && Build.VERSION.SDK_INT<21){
+            setWindowsFlag(this, WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS, true);
+        }
+        if (Build.VERSION.SDK_INT>=19){
+            getWindow().getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_LAYOUT_STABLE |
+                    View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN);
+        }
+        if (Build.VERSION.SDK_INT>=21)
+        {
+            setWindowsFlag(this, WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS, false);
+            getWindow().setStatusBarColor(Color.TRANSPARENT);
+        }
 
         greetings = (TextView)findViewById(R.id.greet);
 
@@ -75,7 +91,7 @@ public class HomeDashBoard extends AppCompatActivity {
             greetings.setText("Magandang Hapon,");
         }
         else if(james >= 17 && james < 24) {
-            greetings.setText("Magandang gabi,");
+            greetings.setText("Magandang Gabi,");
         }
         else{
             greetings.setText("Kamusta ka,");
